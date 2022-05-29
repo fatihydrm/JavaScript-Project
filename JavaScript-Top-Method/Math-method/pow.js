51f4e310f33234eea042ccda57833efd98953bab
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-05-29 19:50:08 +0300

    Fazladan konulmuş 1 düzeltildi ve sonuç eklendi

--- JavaScript-Top-Method/Math-method/pow.js
+++ JavaScript-Top-Method/Math-method/pow.js
@@ -1,4 +1,4 @@
 // Math.pow() Solda belirtilen rakamın sağdaki kadar üssünü alır.
 //(birinci sayıyı ) (ikinci sayı) kadar çarpıyoruz
 
-console.log(Math.pow(3, 4)); // 3*3*3*31
+console.log(Math.pow(3, 4)); // 3*3*3*3, Sonuç: 81
