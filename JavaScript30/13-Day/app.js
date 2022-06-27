9c56ec324d692fa5479105f1838ae752f7b671e4
Fatih Şen <fatihydrm@outlook.com>
2022-06-27 13:38:20 +0300

    .

--- JavaScript30/13-Day/app.js
+++ JavaScript30/13-Day/app.js
@@ -1,1 +1,12 @@
 let scrollElements = document.querySelectorAll(".inner");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const test1 = (window.scrollY / 3).toFixed();
+  console.count(window.scrollTo , window.scrollTo);
+}
+
+scrollElements.forEach((inner) => {});
