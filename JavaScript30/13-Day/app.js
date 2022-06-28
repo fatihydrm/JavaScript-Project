3203066ead15d24c9da3a7f725372c988ea5ad60
Fatih Şen <fatihydrm@outlook.com>
2022-06-29 02:03:56 +0300

    finish day 13 :white_check_mark:

--- JavaScript30/13-Day/app.js
+++ JavaScript30/13-Day/app.js
@@ -1,12 +1,36 @@
-let scrollElements = document.querySelectorAll(".inner");
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  const test1 = (window.scrollY / 3).toFixed();
-  console.count(window.scrollTo , window.scrollTo);
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
-scrollElements.forEach((inner) => {});
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    // half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
