d6d82870f1c43ecf02176825279c53ab5cfe21d8
Fatih Şen <fatihydrm@outlook.com>
2022-06-30 09:18:57 +0300

    .

---
+++ JavaScript30/15-Day/app.js
@@ -0,0 +1,33 @@
+let form = document.querySelector("#form");
+let label = document.querySelectorAll(".label");
+let list = document.querySelector(".list");
+form.children[0].focus();
+function addItem() {
+  let item = document.createElement("li");
+
+  let html = `
+    <input type="checkbox" id="item2" />
+          <label class="label" for="item2">${form.children[0].value}</label>
+          <div class="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path
+                d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"
+              />
+            </svg>
+          </div>
+  `;
+  item.innerHTML = html;
+  list.appendChild(item);
+  form.children[0].value = "";
+  form.children[0].focus();
+}
+
+list.addEventListener("click", (e) => {
+  console.log(e.target);
+});
