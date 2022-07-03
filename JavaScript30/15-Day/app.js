fc4f017e4fe132be63a1e7cea5d081492218f5d6
Fatih Şen <fatihydrm@outlook.com>
2022-07-03 17:15:12 +0300

    finish day 15 ✅

--- JavaScript30/15-Day/app.js
+++ JavaScript30/15-Day/app.js
@@ -32,9 +32,9 @@ let items = [
 
 function html(item, i) {
   let html = `
-  <input type="checkbox" ${checkedControl} id="item${i}" />
+  <input  onchange="checkedtest(${i})" class="checkbox" type="checkbox" ${checkedControl} id="item${i}" />
   <label class="label" for="item${i}">${item.name}</label>
-  <div class="icon">
+  <button class="icon"  onclick="deleteItem(${i})">
   <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
@@ -46,7 +46,7 @@ function html(item, i) {
                d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"
                />
                </svg>
-               </div>
+               </button>
                `;
   let li = document.createElement("li");
   li.innerHTML = html;
@@ -74,9 +74,8 @@ if (localStorage.getItem("data") == null) {
 form.addEventListener("click", (e) => {
   e.preventDefault();
 });
-// add item
 form.children[1].addEventListener("click", () => {
-  if (form.children[0].value !== "") {
+  if (form.children[0].value !== "" && form.children[0].value.length <= 25) {
     list.innerHTML = "";
     itemsParse.push({ name: form.children[0].value, checked: false });
     localStorage.setItem("data", JSON.stringify(itemsParse));
@@ -84,4 +83,21 @@ form.children[1].addEventListener("click", () => {
     form.children[0].value = "";
     form.children[0].focus();
   }
+});
+function deleteItem(i) {
+  itemsParse.splice(i, 1);
+  localStorage.setItem("data", JSON.stringify(itemsParse));
+  list.innerHTML = "";
+  listed();
+}
+function checkedtest(index) {
+  let checkbox = document.querySelectorAll(".checkbox");
+  itemsParse[index].checked = checkbox[index].checked;
+  localStorage.setItem("data", JSON.stringify(itemsParse));
+}
+let clearAll = document.querySelector("#clearAll");
+clearAll.addEventListener("click", () => {
+  itemsParse.splice(0, itemsParse.length);
+  localStorage.setItem("data", JSON.stringify(itemsParse));
+  list.innerHTML = "";
 });
